b0875ceb573405a43bdf05f68d9d1fc65d9ba76b
muzammil2208 <muzammilsk90@gmail.com>
2023-03-30 23:36:10 +0530

    added register page bootstrap

--- index.js
+++ index.js
@@ -8,11 +8,25 @@ const cookie=require('cookie-parser');
 //SETTING 
 app.set('view engine','ejs');
 app.set('views','./views');
-app.use('/js',express.static(__dirname+'./public/js'))
-app.use('/css',express.static(__dirname+'./public/css'))
 
+app.use(express.json());       
+app.use(express.urlencoded({extended: true})); 
+app.use(express.static(__dirname + '/public'));
+ 
+app.get('/',(req,res)=>{
+    res.render('index');
+})
 
+app.get('/register',(req,res)=>{
+    res.render('register')
+})
 
+app.post('/register',(req,res)=>{
+    console.log(req.body.username);
+    console.log(req.body.email);
+    console.log(req.body.password);
+    res.redirect('/register');
+})
 
 app.listen(port,()=>{
     console.log(`server listening at port:${port}`);
--- routes/db-config.js
+++ routes/db-config.js
@@ -1,9 +1,12 @@
-const db=require('mysql')
+const sql=require('mysql')
+const dotenv=require('dotenv').config();
 
-db.createConnection({
-    host:process.env.DATABASE_HOST,
-    user:process.env.DATABASE_USER,
-    password:process.env.DATABASE_PASSWORD,
-    database:process.env.DATABASE_NAME
+const db=sql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME
 })
+
+
 module.exports=db;
